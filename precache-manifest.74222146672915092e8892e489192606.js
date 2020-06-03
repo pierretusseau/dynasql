b271a043dddb5d9b804dd6517a917da2ff828e58
Pierre <pierre@iMac-de-Pierre.local>
2020-06-03 14:31:40 +0200

    Updates

---
+++ precache-manifest.74222146672915092e8892e489192606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b825ac7a06eddbcef11ee3ac84af838",
+    "url": "/dynasql/index.html"
+  },
+  {
+    "revision": "f75f493da8de85d1ecf9",
+    "url": "/dynasql/static/css/main.214844a4.chunk.css"
+  },
+  {
+    "revision": "a04f1278caaba44f738b",
+    "url": "/dynasql/static/js/2.1c0ab909.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dynasql/static/js/2.1c0ab909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75f493da8de85d1ecf9",
+    "url": "/dynasql/static/js/main.6580b920.chunk.js"
+  },
+  {
+    "revision": "0e23c85e696d87337664",
+    "url": "/dynasql/static/js/runtime-main.1b3f1f75.js"
+  }
+]);
